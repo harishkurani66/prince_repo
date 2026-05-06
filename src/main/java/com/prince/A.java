6aeee8882b2ae573bd364f370a9727844f6c89a1
harishkurani66 <harishkurani33@gmail.com>
2026-05-06 21:37:05 +0530

    Added A

---
+++ src/main/java/com/prince/A.java
@@ -0,0 +1,20 @@
+package com.prince;
+
+public class A {
+
+    int a = 20;
+    int b = 30;
+
+    public static void main(String[] args){
+        A a1 = new A();
+
+        System.out.println("100");
+
+       a1.test1();
+
+    }
+
+    public void test1(){
+        System.out.println("Harish");
+    }
+}
